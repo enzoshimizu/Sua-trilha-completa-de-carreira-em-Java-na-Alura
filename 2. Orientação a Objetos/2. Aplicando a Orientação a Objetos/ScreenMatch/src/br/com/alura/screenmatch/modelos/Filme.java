e1c32781d612f89efbd58a1d0e754b21bbb36462
enzoshimizu <eshimizu@gmail.com>
2025-03-15 22:56:29 -0300

    Aula 3

---
+++ 2. Orientação a Objetos/2. Aplicando a Orientação a Objetos/ScreenMatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,15 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Filme extends Titulo {
+
+    private String diretor;
+
+    public String getDiretor() {
+        return this.diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+}
