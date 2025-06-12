5d5f86e48c9ba02ec43491e156a70028fbf739c7
enzoshimizu <eshimizu@gmail.com>
2025-06-12 14:14:55 -0300

    Ponto

--- 3. Crie aplicações usando Spring Boot/1. Criando uma aplicação Web/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ 3. Crie aplicações usando Spring Boot/1. Criando uma aplicação Web/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -3,12 +3,15 @@ package br.com.alura.screenmatch.principal;
 import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -35,8 +38,26 @@ public class Principal {
 			temporadas.add(dadosTemporada);
 		}
 
-		temporadas.forEach(System.out::println);
 
-        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+        System.out.println("---------------------");
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t->t.episodios().stream())
+                .collect(Collectors.toList());
+
+        dadosEpisodios.stream()
+                .filter(e->!e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+        System.out.println("---------------------");
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numero(), d)))
+                .collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
     }
 }
