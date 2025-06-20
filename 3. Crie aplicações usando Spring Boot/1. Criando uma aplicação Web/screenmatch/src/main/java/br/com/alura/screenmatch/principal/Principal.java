54061a07e4e4243c9e36914e9b7f8f4c77d6d063
enzoshimizu <eshimizu@gmail.com>
2025-06-20 09:21:45 -0300

    commit

--- 3. Crie aplicações usando Spring Boot/1. Criando uma aplicação Web/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ 3. Crie aplicações usando Spring Boot/1. Criando uma aplicação Web/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -7,10 +7,9 @@ import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Principal {
@@ -32,7 +31,7 @@ public class Principal {
 
 		List<DadosTemporada> temporadas = new ArrayList<>();
 
-		for (int i = 1; i < dados.totalTemporadas(); i++) {
+		for (int i = 1; i <= dados.totalTemporadas(); i++) {
 			json = consumo.obterDados(ENDERECO + nomeSerie.replaceAll(" ","+") +  "&season=" + i + API_KEY);
 			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
 			temporadas.add(dadosTemporada);
@@ -45,13 +44,13 @@ public class Principal {
                 .flatMap(t->t.episodios().stream())
                 .collect(Collectors.toList());
 
-        dadosEpisodios.stream()
-                .filter(e->!e.avaliacao().equalsIgnoreCase("N/A"))
-                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
-                .limit(5)
-                .forEach(System.out::println);
+//        dadosEpisodios.stream()
+//                .filter(e->!e.avaliacao().equalsIgnoreCase("N/A"))
+//                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+//                .limit(5)
+//                .forEach(System.out::println);
 
-        System.out.println("---------------------");
+//        System.out.println("---------------------");
 
         List<Episodio> episodios = temporadas.stream()
                 .flatMap(t -> t.episodios().stream()
@@ -59,5 +58,48 @@ public class Principal {
                 .collect(Collectors.toList());
 
         episodios.forEach(System.out::println);
+
+//        System.out.println("Digite o trecho do título: ");
+//        String trechoTitulo = leitura.next();
+//
+//        Optional<Episodio> episodioBuscado = episodios.stream()
+//                .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
+//                .findFirst();
+//
+//        System.out.println(episodioBuscado.isPresent() ? episodioBuscado:"Não encontrei");
+
+//        System.out.println("---------------------");
+//
+//        System.out.println("A partir de que ano vocês deseja ver os episódios?");
+//
+//        Integer ano = leitura.nextInt();
+//        leitura.nextLine();
+//
+//        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+//
+//        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//
+//        episodios.stream()
+//                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+//                .forEach(e -> System.out.println(
+//                        "Temporada: " + e.getTemporada() +
+//                                " Episódio: " + e.getTitulo() +
+//                                " Data Lançamento: " + e.getDataLancamento().format(formatador)
+//                ));
+
+        Map<Integer, Double> avaliacoesPorTemporada = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada, Collectors.averagingDouble(Episodio::getAvaliacao)));
+
+        System.out.println(avaliacoesPorTemporada);
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+
+        System.out.println("Media: " + est.getAverage());
+        System.out.println("Melhor: " + est.getMax());
+        System.out.println("Pior: " + est.getMin());
+        System.out.println("Quantidade: " + est.getCount());
     }
 }
